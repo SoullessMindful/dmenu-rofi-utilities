3a8a894292ee530b380e7ef840e07a6208da7719
SoullessMindful <soullessmindful@gmail.com>
2025-07-29 18:14:23 +0200

    List menu extended.

--- main.sh
+++ main.sh
@@ -195,65 +195,101 @@ list_menu() {
 
 list_inner_menu() {
   local list_name="$1"
-  local list_path="$list_directory/$list_name"
-  local all_list_items="📃 Add item"
-  local list_items=$(cat "$list_path")
-  
-  if [[ -n "$list_items" ]]; then
-    all_list_items+=$'\n'
-    all_list_items+=$list_items
-  fi
-
-  local list_item=$(
-    echo "$all_list_items" |
-    $LAUNCHER -i\
-    -p "$list_name"
+  local list_path=$(
+    find "$list_directory"\
+    -type f -name "$list_name"
   )
+  if [[ -n "$list_path" ]]; then
+    local all_list_items="📃 Add item"
+    local list_items=$(cat "$list_path")
+    
+    if [[ -n "$list_items" ]]; then
+      all_list_items+=$'\n'
+      all_list_items+=$list_items
+    fi
 
-  case $list_item in
-    "📃 Add item")
-      local item_content="$(
-        $LAUNCHER -i -p "Item content"
-      )"
+    all_list_items+="\n📝 Rename list\n📝 Edit list\n🗑️ Remove list\n↩️ Back"
 
-      if [[ -n "$item_content" ]]; then
-        echo "$item_content" >> "$list_path"
-      fi
+    local list_item=$(
+      echo -e "$all_list_items" |
+      $LAUNCHER -i\
+      -p "$list_name"
+    )
 
-      list_inner_menu "$list_name"
-      ;;
-    *)
-      local action=$(
-        # edit remove or back
-        echo -e "📝 Edit item\n🗑️ Remove item\n↩️ Back" |
-        $LAUNCHER -i -p "$list_item"
-      )
-      case $action in
-        "📝 Edit item")
-          local new_content="$(
-            $LAUNCHER -i -p "New content for $list_item"
-          )"
-          if [[ -n "$new_content" ]]; then
-            sed -i "s/^$list_item\$/$new_content/" "$list_path"
-          fi
+    case $list_item in
+      "📃 Add item")
+        local item_content="$(
+          $LAUNCHER -i -p "Item content"
+        )"
+
+        if [[ -n "$item_content" ]]; then
+          echo "$item_content" >> "$list_path"
+        fi
+
+        list_inner_menu "$list_name"
+        ;;
+      "📝 Rename list")
+        local new_list_name="$(
+          $LAUNCHER -i -p "New name"\
+          -filter "$list_name"
+        )"
+        
+        if [[ -n "$new_list_name" ]]; then
+          local new_list_path="$list_directory/$new_list_name"
+          mv "$list_path" "$new_list_path"
+          list_inner_menu "$new_list_name"
+        else
           list_inner_menu "$list_name"
-          ;;
-        "🗑️ Remove item")
-          sed -i "/^$list_item$/d" "$list_path"
-          list_inner_menu "$list_name"
-          ;;
-        "↩️ Back")
-          list_menu
-          ;;
-      esac
-      ;;
-  esac
+        fi
+        ;;
+      "📝 Edit list")
+        $TERMINAL --class floating $EDITOR "$list_path"
+        list_inner_menu "$list_name"
+        ;;
+      "🗑️ Remove list")
+        rm "$list_path"
+        list_menu
+        ;;
+      "↩️ Back")
+        list_menu
+        ;;
+      "");;
+      *)
+        if grep -q "^$list_item\$" "$list_path"; then
+          local action=$(
+            echo -e "📝 Rename item\n🗑️ Remove item\n↩️ Back" |
+            $LAUNCHER -i -p "$list_item"
+          )
+          case $action in
+            "📝 Rename item")
+              local new_content="$(
+                $LAUNCHER -i -p "New item content"\
+                -filter "$list_item"
+              )"
+              if [[ -n "$new_content" ]]; then
+                sed -i "s/^$list_item\$/$new_content/" "$list_path"
+              fi
+              list_inner_menu "$list_name"
+              ;;
+            "🗑️ Remove item")
+              sed -i "/^$list_item$/d" "$list_path"
+              list_inner_menu "$list_name"
+              ;;
+            "");;
+            "↩️ Back" | *)
+              list_inner_menu "$list_name"
+              ;;
+          esac
+        fi
+        ;;
+    esac
+  fi
 }
 
 list_add() {
   local list_name="$(
     $LAUNCHER -i -p "List name"
-  ).txt"
+  )"
   local list_path="$list_directory/$list_name"
   touch "$list_path"
 
